2727c04b956374fcc3d83e50e3aec4b7f1922edb
LukeJYK <809698350@qq.com>
2021-07-17 23:50:36 +0800

    add archieve

--- _site/assets/js/zoom.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-    mediumZoom('[data-zoomable]', {
-        margin: 100,
-        background: getComputedStyle(document.documentElement)
-            .getPropertyValue('--global-bg-color') + 'ee',
-    })
-});
